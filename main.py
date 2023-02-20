16197b53ec51f4d52fa3d879091dc5ec321720b6
NicPWNs <nic@nicpjones.com>
2023-02-20 17:53:21 -0500

    Fix guild

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ async def on_member_join(member):
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
-    await tree.sync(guild=discord.Object(id=guild.id))
+    await tree.sync(guild=guild)
 
 client.run(TOKEN)
