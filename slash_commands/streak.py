39544686d9b27619014f1fd59bfa79625be138e7
NicPWNs <nic@nicpjones.com>
2023-02-22 23:09:02 -0500

    feat: add /streak

--- slash_commands/streak.py
+++ slash_commands/streak.py
@@ -1,6 +1,7 @@
+import requests
 import boto3
 from boto3.dynamodb.conditions import Key
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 async def streak(ctx, stats):
@@ -25,7 +26,7 @@ async def streak(ctx, stats):
 
     data = table.get_item(
         Key={
-            'id': str(139927805117857791)
+            'id': str(ctx.user.id)
         }
     )
 
@@ -38,4 +39,167 @@ async def streak(ctx, stats):
         storedNextMid = datetime.fromisoformat(data['Item']['nextMid'])
         storedSkipMid = datetime.fromisoformat(data['Item']['skipMid'])
 
-    await ctx.edit(content=f"{storedNextMid}")
+    prefix = ""
+    streak = 0
+    personalRecord = 0
+
+    currTime = datetime.now()
+    lastMidnight = datetime.combine(currTime.date(), datetime.min.time())
+    nextMidnight = lastMidnight + timedelta(days=1)
+    skipMidnight = lastMidnight + timedelta(days=2)
+
+    if dataLength < 5:
+        prefix = "You just started a new streak! "
+
+        put = table.put_item(
+            Item={
+                'id': str(ctx.user.id),
+                'username': str(ctx.user.name),
+                'streak': str(int(streak) + 1),
+                'updated': str(currTime),
+                'lastMid': str(lastMidnight),
+                'nextMid': str(nextMidnight),
+                'skipMid': str(skipMidnight),
+                'personalRecord': str(int(personalRecord) + 1)
+            }
+        )
+
+        streak = streak + 1
+
+    elif currTime > storedSkipMid:
+        prefix = "You missed your streak! "
+
+        put = table.put_item(
+            Item={
+                'id': str(ctx.user.id),
+                'username': str(ctx.user.name),
+                'streak': str(int(streak) + 1),
+                'updated': str(currTime),
+                'lastMid': str(lastMidnight),
+                'nextMid': str(nextMidnight),
+                'skipMid': str(skipMidnight),
+                'personalRecord': str(data['Item']['personalRecord'])
+            }
+        )
+
+        streak = streak + 1
+
+        if str(ctx.user.id) == str(dataCurrent['Item']['userId']):
+            put = table.put_item(
+                Item={
+                    'id': 'currentStreak',
+                    'stat': '0',
+                    'username': 'MEGABOT',
+                    'userId': '1077260321833635941'
+                }
+            )
+
+            prefix = prefix + "**Current Highest Streak Reset!** "
+
+    elif currTime > storedLastMid and currTime < storedNextMid:
+        prefix = ""
+
+        streak = int(data['Item']['streak'])
+
+        put = table.put_item(
+            Item={
+                'id': str(ctx.user.id),
+                'username': str(ctx.user.name),
+                'streak': str(streak),
+                'updated': str(currTime),
+                'lastMid': str(data['Item']['lastMid']),
+                'nextMid': str(data['Item']['nextMid']),
+                'skipMid': str(data['Item']['skipMid']),
+                'personalRecord': str(data['Item']['personalRecord'])
+            }
+        )
+
+    elif currTime > storedLastMid and currTime > storedNextMid:
+        prefix = "You hit your streak! "
+
+        streak = int(data['Item']['streak']) + 1
+        personalRecord = int(data['Item']['personalRecord'])
+
+        if streak > personalRecord:
+            personalRecord = streak
+
+        put = table.put_item(
+            Item={
+                'id': str(ctx.user.id),
+                'username': str(ctx.user.name),
+                'streak': str(streak),
+                'updated': str(currTime),
+                'lastMid': str(lastMidnight),
+                'nextMid': str(nextMidnight),
+                'skipMid': str(skipMidnight),
+                'personalRecord': str(personalRecord)
+            }
+        )
+
+        if streak > int(dataStats['Item']['stat']):
+            put = table.put_item(
+                Item={
+                    'id': 'allTimeStreak',
+                    'stat': str(streak),
+                    'username': str(ctx.user.name),
+                    'userId': str(ctx.user.id)
+                }
+            )
+
+        if streak > int(dataCurrent['Item']['stat']):
+            put = table.put_item(
+                Item={
+                    'id': 'currentStreak',
+                    'stat': str(streak),
+                    'username': str(ctx.user.name),
+                    'userId': str(ctx.user.id)
+                }
+            )
+
+    if streak < 2:
+        emote = "💩"
+    elif streak < 3:
+        emote = "✌️"
+    elif streak < 4:
+        emote = "👌"
+    elif streak < 5:
+        emote = "🍀"
+    elif streak < 10:
+        emote = "🔥"
+    elif streak < 25:
+        emote = "🧨"
+    elif streak < 50:
+        emote = "🏆"
+    elif streak < 69:
+        emote = "💀"
+    elif streak < 70:
+        emote = "-  *nice*."
+    elif streak < 75:
+        emote = "💀"
+    elif streak < 100:
+        emote = "💎"
+    else:
+        emote = "💯"
+
+    statMessage = ""
+
+    if stats == "True":
+        statMessage = "\n\n📊  __**Streak Stats**__\n**All-Time Highest Streak:** " + \
+            str(dataStats['Item']['stat']) + \
+            " *by* <@" + \
+            str(dataStats['Item']['userId']) + \
+            ">" + \
+            "\n**Current Highest Streak:** " + \
+            str(dataCurrent['Item']['stat']) + \
+            " *by* <@" + \
+            str(dataCurrent['Item']['userId']) + \
+            ">" + \
+            "\n**Personal Highest Streak:** " + \
+            str(data['Item']['personalRecord']) + \
+            " *by* <@" + \
+            str(ctx.user.id) + ">"
+
+    content = prefix + "Your streak is: " + \
+        str(streak) + "   " + emote + statMessage
+
+    await ctx.edit(content=f"{content}")
