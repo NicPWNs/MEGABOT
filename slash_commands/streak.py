c0b176021329f103bb8bea270f13b128bb7697aa
NicPWNs <nic@nicpjones.com>
2023-02-22 20:14:28 -0500

    feat: add /streak

--- slash_commands/streak.py
+++ slash_commands/streak.py
@@ -1,10 +1,21 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 
 
 async def streak(ctx, stats):
 
     await ctx.respond(content="*⏳ Loading...*")
 
-    content = "Good!"
+    TABLE = "discord-streak"
+    ddb = boto3.resource('dynamodb')
+    table = ddb.Table(TABLE)
+
+    response = table.get_item(
+        Key={
+            'id': 'allTimeStreak'
+        }
+    )
+
+    content = response['Item']
 
     await ctx.edit(content=f"{content}")
