80a195436244055671715ac013dc50b79e131978
NicPWNs <nic@nicpjones.com>
2023-02-26 14:32:18 -0500

    fix: restructure

--- megabot.py
+++ megabot.py
@@ -4,8 +4,6 @@ import discord
 from discord import option
 from dotenv import load_dotenv
 
-from listeners.on_message import on_message
-from listeners.on_member_join import on_member_join
 from slash_commands.age import age
 from slash_commands.bless import bless
 from slash_commands.chat import chat
@@ -26,12 +24,28 @@ if __name__ == "__main__":
     bot = discord.Bot(intents=discord.Intents.all())
 
     @bot.listen('on_message')
-    async def call(message):
-        on_message(message, bot)
+    async def on_message(message):
+
+        if message.author == bot.user:
+            return
+
+        if 'birthday' in message.content.lower():
+            await message.channel.send('Happy Birthday! 🎈🎉')
+
+        if 'megabot' in message.content.lower():
+            await message.channel.send('Hello there! 👋')
 
     @bot.listen('on_member_join')
-    async def call(member):
-        on_member_join(member, bot)
+    async def on_member_join(member):
+
+        guild = discord.utils.get(bot.guilds, name="MEGACORD")
+
+        role = discord.utils.get(guild.roles, name="MEGAENJOYERS")
+        await member.add_roles(role)
+
+        channel = discord.utils.get(guild.channels, name="main")
+
+        await channel.send(f"I'm watching you <@{member.id}>")
 
     @bot.slash_command(name="ping", description="Responds with pong.", guild_ids=[GUILD_ID])
     async def call(ctx):
