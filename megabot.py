962c5cf6a96907326fa369ebb8e369141b8a14bb
NicPWNs <nic@nicpjones.com>
2023-02-26 14:56:21 -0500

    feat: /test

--- megabot.py
+++ megabot.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     async def on_message(message):
 
         if 'testing its event listeners' in message.content.lower():
-            await message.channel.send('✅ MEGABOT Testing Done!')
+            await message.channel.send('\n\n✅ MEGABOT Testing Done!')
 
         if message.author == bot.user:
             return
