38f5a0885edca81b18c8549ff2fe55bb098ce3dc
NicPWNs <nic@nicpjones.com>
2023-02-26 14:58:47 -0500

    feat: /test

--- slash_commands/test.py
+++ slash_commands/test.py
@@ -14,7 +14,7 @@ async def test(ctx, startTime):
 
     test1 = f"⚙️ MEGABOT is running on {hostname} (PID {pid})\n\n"
     test2 = f"🐍 MEGABOT is running on Python v{py}\n\n"
-    test3 = f"⏲️ MEGABOT has been running for {runTime}s\n\n"
+    test3 = f"⏲️ MEGABOT has been running for {runTime}s\n\n‏"
     test4 = f"👂 MEGABOT is testing its event listeners:"
 
     content = test1 + test2 + test3 + test4
