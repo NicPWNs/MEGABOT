b18ea6a27652b01d294ac4637fcbc1fecc88aa9e
NicPWNs <nic@nicpjones.com>
2023-02-26 15:05:40 -0500

    feat: /test

--- slash_commands/test.py
+++ slash_commands/test.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import time
+import datetime
 import platform
 
 
@@ -10,12 +11,12 @@ async def test(ctx, startTime):
     hostname = platform.node()
     pid = os.getpid()
     py = sys.version[0:3]
-    runTime = int(time.time() - startTime)
+    runTime = str(datetime.timedelta(seconds=int(time.time() - startTime)))
 
     test1 = f"⚙️ MEGABOT is running on {hostname} (PID {pid})\n\n"
     test2 = f"🐍 MEGABOT is running on Python v{py}\n\n"
     test3 = f"⏲️ MEGABOT has been running for {runTime}s\n\n"
-    test4 = f"👂 MEGABOT is testing its event listeners:\n‏"
+    test4 = f"👂 MEGABOT is testing its *event listeners*:\n‏"
 
     content = test1 + test2 + test3 + test4
 
