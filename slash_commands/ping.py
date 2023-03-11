336f81213aeae7b53e01e5664e98c776604410fc
NicPWNs <nic@nicpjones.com>
2023-03-11 13:49:24 -0500

    feat: add more embeds

--- slash_commands/ping.py
+++ slash_commands/ping.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+import discord
 
 
 async def ping(ctx):
-    await ctx.respond(content="Pong! 🏓")
+
+    embed = discord.Embed(color=0xffac33, title="🏓  Pong!").set_thumbnail(url="https://raw.githubusercontent.com/NicPWNs/MEGABOT/main/thumbnail.gif")
+
+    await ctx.respond(embed=embed)
