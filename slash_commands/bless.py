81c48f1bf1a66d5862bc03a59f70ba04fcc49bfe
NicPWNs <nic@nicpjones.com>
2023-03-11 13:46:00 -0500

    feat: add more embeds

--- slash_commands/bless.py
+++ slash_commands/bless.py
@@ -4,6 +4,6 @@ import discord
 
 async def bless(ctx):
 
-    embed = discord.Embed(color=0xffac33, title="✨  The mess has been blessed!").set_thumbnail(url="https://user-images.githubusercontent.com/23003787/222802744-34fa733e-32d2-4093-be09-7a9d433f9df2.gif")
+    embed = discord.Embed(color=0xffac33, title="✨  The mess has been blessed!").set_thumbnail(url="https://raw.githubusercontent.com/NicPWNs/MEGABOT/main/thumbnail.gif")
 
     await ctx.respond(embed=embed)
