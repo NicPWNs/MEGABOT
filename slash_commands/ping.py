915561dad314a931e1d1608fc419634acbb160ce
NicPWNs <nic@nicpjones.com>
2023-03-11 13:50:12 -0500

    feat: add more embeds

--- slash_commands/ping.py
+++ slash_commands/ping.py
@@ -4,6 +4,6 @@ import discord
 
 async def ping(ctx):
 
-    embed = discord.Embed(color=0xffac33, title="🏓  Pong!").set_thumbnail(url="https://raw.githubusercontent.com/NicPWNs/MEGABOT/main/thumbnail.gif")
+    embed = discord.Embed(color=0xdc2e45, title="🏓  Pong!").set_thumbnail(url="https://raw.githubusercontent.com/NicPWNs/MEGABOT/main/thumbnail.gif")
 
     await ctx.respond(embed=embed)
