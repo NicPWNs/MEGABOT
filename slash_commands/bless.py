9260f23069c2958161023cdf11ccb035297b19da
NicPWNs <nic@nicpjones.com>
2023-03-11 13:20:53 -0500

    feat: add more embeds

--- slash_commands/bless.py
+++ slash_commands/bless.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+import discord
 
 
 async def bless(ctx):
-    await ctx.respond(content="The mess has been blessed! ✨")
+
+    embed = discord.Embed(color=0xffac33, title="✨  The mess has been blessed!").set_thumbnail(url=ctx.bot.user.avatar.url)
+
+    ctx.respond(embed=embed)
