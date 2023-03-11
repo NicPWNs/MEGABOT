6a948365b61748406801b3d95a30743316e6f58e
NicPWNs <nic@nicpjones.com>
2023-03-11 12:56:02 -0500

    feat: add caption to /image

--- slash_commands/image.py
+++ slash_commands/image.py
@@ -28,7 +28,7 @@ async def image(ctx, prompt):
 
     url = r.data[0].url
 
-    embed = discord.Embed(color=0x5965f3, description=f"<@{ctx.user.id}>").set_image(url=url).set_footer(text=f"\" {prompt} \" - <@{ctx.user.id}>").set_author(name=f"<@{ctx.user.id}>")
+    embed = discord.Embed(color=0x5965f3, title=f"\" {prompt} \"", description=f"by <@{ctx.user.id}>").set_image(url=url)
 
     await interaction.edit_original_response(embed=embed)
 
