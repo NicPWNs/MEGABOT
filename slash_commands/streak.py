c4a9b534105e85dfae210364708b01e4f7ea23ca
NicPWNs <nic@nicpjones.com>
2023-03-22 20:16:16 -0400

    deps: bump random-unicode-emoji version

--- slash_commands/streak.py
+++ slash_commands/streak.py
@@ -188,6 +188,8 @@ async def streak(ctx, stats):
     else:
         guild = discord.utils.get(ctx.bot.guilds, name="MEGACORD")
         emojis = await guild.fetch_emojis()
+        for _ in range(0,6):
+            emojis = emojis + emojis
         emote = str(await random_emoji(custom=emojis)[0])
 
     statMessage = ""
