af471993f19ae168e5eade62d7341b0d72ada8ef
NicPWNs <nic@nicpjones.com>
2023-03-29 16:16:48 -0400

    feat: add /double

--- slash_commands/double.py
+++ slash_commands/double.py
@@ -8,11 +8,11 @@ import megacoin
 async def double(ctx, confirm):
 
     if not confirm:
-        embed = discord.Embed(color=0xf6be3c, title="⚖️  Double or Nothing")
+        embed = discord.Embed(color=0xffad32, title="⚖️  Double or Nothing")
         await ctx.respond(embed=embed)
         return
 
-    embed = discord.Embed(color=0xf6be3c, title="⚖️  Double or Nothing").set_thumbnail(
+    embed = discord.Embed(color=0xffad32, title="⚖️  Double or Nothing").set_thumbnail(
         url="https://raw.githubusercontent.com/NicPWNs/MEGABOT/main/images/double.gif")
     interaction = await ctx.respond(embed=embed)
 
@@ -21,17 +21,17 @@ async def double(ctx, confirm):
     win = random.choice([0, 1])
 
     if win:
-        description = "You Win!"
+        description = f"<@{ctx.user.id}> Won!"
         url = "https://raw.githubusercontent.com/NicPWNs/MEGABOT/main/images/win.png"
         await megacoin.add(ctx.user, balance)
     else:
-        description = "You Lose!"
+        description = f"<@{ctx.user.id}> Lost!"
         url = "https://raw.githubusercontent.com/NicPWNs/MEGABOT/main/images/lose.png"
         await megacoin.subtract(ctx.user, balance)
 
     time.sleep(random.randint(2, 10))
 
     embed = discord.Embed(
-        color=0xf6be3c, title="⚖️  Double or Nothing", description=description).set_thumbnail(url=url).set_footer(text=f"Your balance is now {await megacoin.balance(ctx.user)}")
+        color=0xffad32, title="⚖️  Double or Nothing", description=description).set_thumbnail(url=url).set_footer(text=f"Their balance is now {await megacoin.balance(ctx.user)}")
 
     await interaction.edit_original_response(embed=embed)
