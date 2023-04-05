be5bfc899dad03f8bc9af5fa4510c1cf99c97b62
NicPWNs <nic@nicpjones.com>
2023-04-05 10:57:18 -0400

    fix: remove playing cards from reactions

--- modules/random_discord_emoji.py
+++ modules/random_discord_emoji.py
@@ -7,13 +7,12 @@ async def random_discord_emoji(guild, bot, discordName):
     guild = discord.utils.get(bot.guilds, name=discordName)
     emojis = await guild.fetch_emojis()
 
+    # Remove playing card emojis
+    emojis = list(
+        filter(lambda a: (len(a.name) == 2 or len(a.name) == 3) and a.name[-1] not in ['c', 'd', 'h', 's'], emojis))
+
     # Increase ratio of Discord emojis
     for _ in range(0, 6):
         emojis = emojis + emojis
 
-    # Remove playing card emojis
-    for emoji in emojis:
-        if len(emoji.name) == 2 and emoji.name[1] in ['c', 'd', 'h', 's']:
-            emojis.remove(emoji)
-
-    return random_emoji(version="14.0", custom=emojis)[0]
+    return random_emoji(version="15.0", custom=emojis)[0]
