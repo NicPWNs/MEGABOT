795e9bd2abab4e97110de1ecc881f5410b8bd750
NicPWNs <nic@nicpjones.com>
2024-06-05 18:04:01 -0400

    feat: improve /version

--- commands/version.py
+++ commands/version.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import discord
 from github import Github
 
 
@@ -8,7 +9,11 @@ async def version(ctx):
     G = Github(os.getenv("GITHUB_TOKEN"))
     repo = G.get_repo("NicPWNs/MEGABOT")
     release = repo.get_releases()[0].title
-    # TODO: Add Link to Release
-    await ctx.respond(
-        content=f"📦  The latest <@{ctx.bot.user.id}> release is **{release}**"
+
+    embed = discord.Embed(
+        color=0xD89B82,
+        title="📦 Current Version",
+        description=f"The latest <@{ctx.bot.user.id}> release is [**{release}**](https://github.com/NicPWNs/MEGABOT/releases)",
     )
+
+    await ctx.respond(embed=embed)
