5ebbcd52213ce22c70a0f5cf43f47c7e2e486c54
NicPWNs <nic@nicpjones.com>
2024-06-17 10:58:09 -0400

    fix: remove /vote

--- megabot.py
+++ megabot.py
@@ -57,7 +57,6 @@ from commands.streak import streak
 from commands.test import test
 from commands.upload import upload
 from commands.version import version
-from commands.vote import vote
 from commands.weather import weather
 from commands.wheel import wheel
 
@@ -692,16 +691,6 @@ if __name__ == "__main__":
     )
     async def call(ctx):
         await version(ctx)
-
-    @bot.slash_command(name="vote", description="Vote daily for MEGACOINS!")
-    async def call(ctx):
-        if not (ctx.channel.name == "casino" or ctx.channel.name == "bot-testing"):
-            await ctx.send_response(
-                content="❗**ERROR: You can only use this command in <#1091083497868886108>**",
-                ephemeral=True,
-            )
-            return
-        await vote(ctx)
 
     @bot.slash_command(name="weather", description="Seven day weather forecast.")
     async def call(
