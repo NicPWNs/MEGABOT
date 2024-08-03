19e22a2ff703dcea9541e391e0e5273a840fc55c
NicPWNs <nic@nicpjones.com>
2024-08-03 19:29:14 -0400

    fix: NavyBoy's mess

--- commands/retirement.py
+++ commands/retirement.py
@@ -1,51 +1,51 @@
 #!/usr/bin/env python3
+import discord
 
 
-async def retirement(ctx, age, startingcash, yearlysavings, desiredincome, growthrate):
+async def retirement(
+    ctx, startingage, startingcash, yearlysavings, desiredincome, growthrate
+):
+
+    # Initial Response
+    embed = discord.Embed(color=0xFEE9B6, title="⏳  Loading...")
+    interaction = await ctx.respond(embed=embed)
 
     sustainingyear = 0
     halfwaymoney = 0
-    testage = 0
+    midage = 0
     ageset = False
-    startingage = int(age)
-    age = int(age)
-    startingcash = float(startingcash)
-    yearlysavings = float(yearlysavings)
-    desiredincome = float(desiredincome)
-    growthrate = float(growthrate)
-
+    age = startingage
     money = startingcash
-
     growthrate = (growthrate / 100) + 1
 
     while age < 60:
         money = (money + yearlysavings) * growthrate
         annualdividends = money * 0.04
 
-        testage = int(((60 - age) / 2) + (startingage))
+        midage = int(((60 - age) / 2) + (startingage))
 
-        if age == testage:
+        if age == midage:
             halfwaymoney = money
 
         if annualdividends >= desiredincome and ageset is False:
             sustainingyear = str(age)
             ageset = True
         age = age + 1
-    investmentworth = money
-
-    # outputs variables are as follows
-    # investmentworth => value of investments at 60 years
-    # halfwaymoney => value of investments halfway to retirement
-    # annualdividends => annual $ from dividends from investments
-    # sustainingyear => user's age when desired yearly income
-    #                   is met by investment dividends.
 
     if sustainingyear == 0:
-        sustainingyear = str("you'll be dead, buddy")
+        sustainingyear = str("You'll be dead, buddy")
 
-    content = f"_______________ \n Investment value at 60 years old => {str(format(int(investmentworth), ',d'))} \n"
-    content += f"Annual Dividends at 60 years old => {str(format(int(annualdividends), ',d'))} \n"
+    content = f"Investment value at 60 years old => {str(format(money, ',d'))} \n"
+    content += (
+        f"Annual dividends at 60 years old => {str(format(annualdividends, ',d'))} \n"
+    )
     content += f"Age you can live off dividends => {str(sustainingyear)} \n"
-    content += f"Investment value halfway to retirement => {str(int(halfwaymoney))} \n"
-    # content += f"Age {str(testage)}"  # USED FOR TESTING.
-    await ctx.respond(content=content)
+    content += f"Investment value halfway to retirement => {str(halfwaymoney)} \n"
+
+    embed = discord.Embed(
+        color=0xD89B82,
+        title="📈 Retirement",
+        description=content,
+    )
+
+    await interaction.edit_original_response(embed=embed)
